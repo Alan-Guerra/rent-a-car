d49cfda9b4c19fa3d5cffb4ebab73dd077cd9986
Alan-Guerra <alanguerra.war@gmail.com>
2024-02-15 22:03:09 -0300

    add User module services

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,14 @@
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './infrastructure-persistance/entities/user.entity';
 import { Module } from '@nestjs/common';
+import { UserService } from './application/service/user.service';
+import { UserController } from './controllers/user.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
-    providers: [],
-    controllers: [],
-    exports: []
+    providers: [UserService],
+    controllers: [UserController],
+    exports: [UserService]
 })
 
 export class UserModule{};
