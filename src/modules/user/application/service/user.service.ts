47b69463588c5335c1adb10aef2c1e8a06e26fa6
Alan-Guerra <alanguerra.war@gmail.com>
2024-02-16 16:49:23 -0300

    Create User create, get and getAll

--- src/modules/user/application/service/user.service.ts
+++ src/modules/user/application/service/user.service.ts
@@ -1,14 +1,26 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { User } from "../../infrastructure-persistance/entities/user.entity";
-
-
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../../infrastructure-persistance/entities/user.entity';
+import { CreateUserDto } from '../../controllers/dto/create-user.dto';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>,
-    ){}
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  createUser(user: CreateUserDto) {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+
+  getAllUsers() {
+    return this.userRepository.find();
+  }
+
+  getUser(id: number) {
+    return this.userRepository.findOne({ where: { id } });
+  }
 }
--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,8 +1,24 @@
-import { Controller } from "@nestjs/common";
-import { UserService } from "../application/service/user.service";
+import { Controller, Post, Body, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from '../application/service/user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from '../infrastructure-persistance/entities/user.entity';
 
-
-@Controller("user")
+@Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  createUser(@Body() newUser: CreateUserDto): Promise<User | null> {
+    return this.userService.createUser(newUser);
+  }
+
+  @Get()
+  getAllUsers(): Promise<User[]>{
+    return this.userService.getAllUsers();
+  }
+
+  @Get(":id")
+  getUser(@Param("id", ParseIntPipe) id: number): Promise<User>{
+    return this.userService.getUser(id);
+  }
 }
